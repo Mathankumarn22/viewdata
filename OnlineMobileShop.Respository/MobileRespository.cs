20a64b5063973a6dae384b0b5b2156c21125573e
Admin <Admin@DESKTOP-RLL3JF9>
2020-02-14 10:00:50 +0530

    Add project files.

---
+++ OnlineMobileShop.Respository/MobileRespository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using OnlineMobileShop.Entity;
+
+namespace OnlineMobileShop.Respository
+{
+    public class MobileRespository
+    {
+        public static List<Mobile> mobile = new List<Entity.Mobile>();
+
+        static MobileRespository()
+        {
+            mobile.Add(new Mobile { MobileID = 1, Brand = "Nokia", Model = "y22", Battery = 3000, Price = 6000 });
+
+            mobile.Add(new Mobile { MobileID = 2, Brand = "Vivo", Model = "v5", Battery = 4000, Price = 8000 });
+
+            mobile.Add(new Mobile { MobileID = 3, Brand = "Oppo", Model = "f1", Battery = 5000, Price = 11000 });
+        }
+        public static IEnumerable<Mobile> GetDetails()
+        {
+            return mobile;
+        }
+    }
+}
+
---
+++ OnlineMobileShop/Controllers/MobileController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using OnlineMobileShop.Entity;
+using OnlineMobileShop.Respository;
+using System.Web.Mvc;
+
+
+namespace OnlineMobileShop.Controllers
+{
+    public class MobileController : Controller
+    {
+        // GET: Mobile
+        public ActionResult MobileDetails()
+        {
+            IEnumerable<Mobile> mobile = MobileRespository.GetDetails();
+            ViewBag.Mobile = mobile;
+            ViewData["mobile"] = mobile;
+            TempData["mobile"] = mobile;
+            return RedirectToAction("DisplayDetails");
+        }
+        public ActionResult DisplayDetails()
+        {
+            return View();
+        }
+    }
+}
